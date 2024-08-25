f16f78d5b4b6cf7ecbffca4970adc913f7f20fa7
Rafael Durán <69222919+Rafael-Dur@users.noreply.github.com>
2024-08-24 23:24:53 -0300

    TA1

---
+++ TA01/script.js
@@ -0,0 +1,13 @@
+
+function repeatString(texto, repeticiones) {
+    let resultado = '';
+    for (let i = 0; i < repeticiones; i++) {
+        resultado += texto + '<br>'
+    }
+    document.getElementById("output").innerHTML = resultado;
+}
+
+// Ejemplo de uso
+document.getElementById.toString
+repeatString("Hola, Rafael", 10);
+
